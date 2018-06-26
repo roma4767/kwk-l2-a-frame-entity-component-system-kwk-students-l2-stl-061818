bd0d6a7db04b0b150086ebd0ad97a41930512636
Romina D-R <rdr2711@yahoo.com>
2018-06-26 19:05:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function createSpheres() {
   
   newSphere2.setAttribute("radius", Math.ceil(Math.random() * 3))
   newSphere2.setAttribute("color", color[Math.random()])
-  newSphere2.setAttribute("position", Math.random())
+  newSphere2.setAttribute("position", startPositions[Math.random()])
   
   return newSphere1
   return newSphere2
